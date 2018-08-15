4ed1ce66162b1742fd2fd35812df0fca9331358c
LindsayMiller <lmm566@gmail.com>
2018-08-14 21:14:47 -0400

    Adds toggle menu test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -69,11 +69,18 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* This test ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        
+        it('change visibility when clicked',function(){
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
 
     });
